67ac770e3e37f123ef5463642b393f0218367d4c
Zulfishka <zulfishka1@gmail.com>
2023-03-24 16:30:31 +0600

    Initial commit

---
+++ app/src/main/java/com/geekthech/lesson25hw/MainActivity.java
@@ -0,0 +1,18 @@
+package com.geekthech.lesson25hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button =  findViewById(R.id.button_next);
+
+    }
+}
